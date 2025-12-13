600a4ce66db3bf56d2278a37460fabacb36562cb
Jay Dev <rip.jaydev@gmail.com>
2025-12-13 16:00:22 -0600

    pushing the aws working download ig reel code with api

--- playground.py
+++ playground.py
@@ -1,8 +1,8 @@
 import requests
 
-url = "https://6tbypxrnfjxfwzthmahds52oky0hpuhc.lambda-url.us-east-1.on.aws/"
+url = "https://24nxpvemp7t7kk2ejbhwhquciu0faoiu.lambda-url.us-east-1.on.aws/"
 payload = {
-    'reel_url': 'https://www.instagram.com/p/DQzJhVck713/'
+    'reel_url': 'https://www.instagram.com/p/DSKilXBDeyq/'
 }
 
 response = requests.post(url, json=payload)
